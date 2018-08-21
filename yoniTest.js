098ed88202b42c8bf9c6c934d8919f1cee41be95
yoni chanowitz <yonichanowitz@gmail.com>
2018-08-21 16:27:34 -0400

    Update yoniTest.js

--- yoniTest.js
+++ yoniTest.js
@@ -2,5 +2,9 @@
 accounting.settings();
 accounting.formatMoney();
 accounting.formatColumn();
-accounting.toFixed()
-accounting.unformat()
+accounting.toFixed();
+accounting.unformat();
+
+var numberToWork = 2378924568234;
+
+console.log(accounting.formatColumn(numberToWork));
